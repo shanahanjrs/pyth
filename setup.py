9656d2d61f11aa689b1bddc9e457dca7d0f1ad3a
John Shanahan <joshanah@cisco.com>
2018-06-07 14:44:07 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyth3",
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(),
     zip_safe=False,
     install_requires=['six'],
